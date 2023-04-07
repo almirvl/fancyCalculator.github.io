ac74f487e0500971c7f6c6473f7356d7fcb39392
almirvl <73787436+almirvl@users.noreply.github.com>
2023-04-08 01:32:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,10 +17,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 document.addEventListener('DOMContentLoaded', () => {
   const audioTracks = [
-    'audio/audio1.mp3',
-    'audio/audio2.mp3',
-    'audio/audio3.mp3',
-    'audio/audio4.mp3',
+    'audio1.mp3',
+    'audio2.mp3',
+    'audio3.mp3',
+    'audio4.mp3',
   ];
 
   const audioToggle = document.getElementById('audio-toggle');
